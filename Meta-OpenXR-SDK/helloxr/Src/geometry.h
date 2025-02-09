33dac88de914b786996c89b7257c96021c66c066
Robert Aldridge <robert@seattleholograms.com>
2025-02-09 02:11:22 -0500

    cleanup

--- Meta-OpenXR-SDK/helloxr/Src/geometry.h
+++ Meta-OpenXR-SDK/helloxr/Src/geometry.h
@@ -11,24 +11,24 @@ struct Vertex
   XrVector3f Color;
 };
 
-constexpr XrVector3f ColorRed {1, 0, 0};
-constexpr XrVector3f ColorHalfRed {0.5, 0, 0};
+constexpr XrVector3f ColorRed {1.0, 0.0, 0.0};
+constexpr XrVector3f ColorHalfRed {0.5, 0.0, 0.0};
 
-constexpr XrVector3f ColorGreen {0, 1, 0};
-constexpr XrVector3f ColorHalfGreen {0, 0.5, 0};
+constexpr XrVector3f ColorGreen {0.0, 1.0, 0.0};
+constexpr XrVector3f ColorHalfGreen {0.0, 0.5, 0.0};
 
-constexpr XrVector3f ColorBlue {0, 0, 1};
-constexpr XrVector3f ColorHalfBlue {0, 0, 0.5};
+constexpr XrVector3f ColorBlue {0.0, 0.0, 1.0};
+constexpr XrVector3f ColorHalfBlue {0.0, 0.0, 0.5};
 
-constexpr XrVector3f ColorYellow {1, 1, 0};
-constexpr XrVector3f ColorHalfYellow {0.5, 0.5, 0};
+constexpr XrVector3f ColorYellow {1.0, 1.0, 0.0};
+constexpr XrVector3f ColorHalfYellow {0.5, 0.5, 0.0};
 
-constexpr XrVector3f ColorCyan {0, 1, 1};
-constexpr XrVector3f ColorHalfCyan {0, 0.5, 0.5};
+constexpr XrVector3f ColorCyan {0.0, 1.0, 1.0};
+constexpr XrVector3f ColorHalfCyan {0.0, 0.5, 0.5};
 
-constexpr XrVector3f ColorPurple {1, 0, 1};
-constexpr XrVector3f ColorHalfPurple {0.5, 0, 0.5};
-constexpr XrVector3f ColorQuarterPurple {0.25, 0, 0.25};
+constexpr XrVector3f ColorPurple {1.0, 0.0, 1.0};
+constexpr XrVector3f ColorHalfPurple {0.5, 0.0, 0.5};
+constexpr XrVector3f ColorQuarterPurple {0.25, 0.0, 0.25};
 
 constexpr XrVector3f ColorOrange {1.0, 0.5, 0.0};
 constexpr XrVector3f ColorHalfOrange {0.5, 0.25, 0.0};
@@ -36,8 +36,8 @@ constexpr XrVector3f ColorHalfOrange {0.5, 0.25, 0.0};
 constexpr XrVector3f ColorBlah {0.0, 0.5, 1.0};
 constexpr XrVector3f ColorHalfBlah {0.0, 0.25, 0.5};
 
-constexpr XrVector3f ColorBlack {0, 0, 0};
-constexpr XrVector3f ColorWhite {1, 1, 1};
+constexpr XrVector3f ColorBlack {0.0, 0.0, 0.0};
+constexpr XrVector3f ColorWhite {1.0, 1.0, 1.0};
 
 constexpr XrVector3f ColorDarkGrey {0.25, 0.25, 0.25};
 constexpr XrVector3f ColorGrey {0.5, 0.5, 0.5};
@@ -53,15 +53,6 @@ constexpr XrVector3f CPPNN {0.5, -0.5, -0.5};
 constexpr XrVector3f CPPNP {0.5, -0.5, 0.5};
 constexpr XrVector3f CPPPN {0.5, 0.5, -0.5};
 constexpr XrVector3f CPPPP {0.5, 0.5, 0.5};
-
-constexpr XrVector3f APNNN {-1.0 / 6.0, -1.0 / 6.0, 0.0};
-constexpr XrVector3f APNNP {-1.0 / 6.0, -1.0 / 6.0, -2.0};
-constexpr XrVector3f APNPN {-1.0 / 6.0, 1.0 / 6.0, 0.0};
-constexpr XrVector3f APNPP {-1.0 / 6.0, 1.0 / 6.0, -2.0};
-constexpr XrVector3f APPNN {1.0 / 6.0, -1.0 / 6.0, 0.0};
-constexpr XrVector3f APPNP {1.0 / 6.0, -1.0 / 6.0, -2.0};
-constexpr XrVector3f APPPN {1.0 / 6.0, 1.0 / 6.0, 0.0};
-constexpr XrVector3f APPPP {1.0 / 6.0, 1.0 / 6.0, -2.0};
 
 constexpr float N2ND {0.70710678118654752440084436210485};
 
@@ -91,23 +82,23 @@ constexpr XrVector3f NNPZ {-N2ND, N2ND, 0.0};
 constexpr XrVector3f NPNZ {N2ND, -N2ND, 0.0};
 constexpr XrVector3f NPPZ {N2ND, N2ND, 0.0};
 
-constexpr XrVector3f NNZZ {-1, 0, 0};
-constexpr XrVector3f NPZZ {1, 0, 0};
+constexpr XrVector3f NNZZ {-1.0, 0.0, 0.0};
+constexpr XrVector3f NPZZ {1.0, 0.0, 0.0};
 
-constexpr XrVector3f NZNZ {0, -1, 0};
-constexpr XrVector3f NZPZ {0, 1, 0};
+constexpr XrVector3f NZNZ {0.0, -1.0, 0.0};
+constexpr XrVector3f NZPZ {0.0, 1.0, 0.0};
 
-constexpr XrVector3f NZZN {0, 0, -1};
-constexpr XrVector3f NZZP {0, 0, 1};
+constexpr XrVector3f NZZN {0.0, 0.0, -1.0};
+constexpr XrVector3f NZZP {0.0, 0.0, 1.0};
 
-constexpr XrVector3f CPZZN {0, 0, -0.5};
-constexpr XrVector3f CPZZP {0, 0, 0.5};
+constexpr XrVector3f CPZZN {0.0, 0.0, -0.5};
+constexpr XrVector3f CPZZP {0.0, 0.0, 0.5};
 
-constexpr XrVector3f CPZNZ {0, -0.5, 0};
-constexpr XrVector3f CPZPZ {0, 0.5, 0};
+constexpr XrVector3f CPZNZ {0.0, -0.5, 0.0};
+constexpr XrVector3f CPZPZ {0.0, 0.5, 0.0};
 
-constexpr XrVector3f CPNZZ {-0.5, 0, 0};
-constexpr XrVector3f CPPZZ {0.5, 0, 0};
+constexpr XrVector3f CPNZZ {-0.5, 0.0, 0.0};
+constexpr XrVector3f CPPZZ {0.5, 0.0, 0.0};
 
 constexpr XrVector3f APZZN {0.0, 0.0, 0.0};
 constexpr XrVector3f APZZP {0.0, 0.0, -2.0};
@@ -118,46 +109,63 @@ constexpr XrVector3f APZPZ {0.0, 1.0 / 6.0, -1.0};
 constexpr XrVector3f APNZZ {-1.0 / 6.0, 0.0, -1.0};
 constexpr XrVector3f APPZZ {1.0 / 6.0, 0.0, -1.0};
 
-// {-0.5, 0.5, -0.5}, {-0.5, 0.5, 0.5}, {-0.5, -0.5, 0.5}
-//
-// {-0.5, 0.5, -0.5}, {-0.5, 0.5, 0.5}, {-0.5, 0, 0}, {-0.5, 0, 0}, {-0.5, 0.5, 0.5}, {-0.5, -0.5, 0.5}
+constexpr XrVector3f APNNN {-1.0 / 6.0, -1.0 / 6.0, 0.0};
+constexpr XrVector3f APNPN {-1.0 / 6.0, 1.0 / 6.0, 0.0};
+constexpr XrVector3f APPNN {1.0 / 6.0, -1.0 / 6.0, 0.0};
+constexpr XrVector3f APPPN {1.0 / 6.0, 1.0 / 6.0, 0.0};
+
+constexpr XrVector3f APN2N2N2 {-1.0 / 2.0, -1.0 / 2.0, -2.0};
+constexpr XrVector3f APN2P2N2 {-1.0 / 2.0, 1.0 / 2.0, -2.0};
+constexpr XrVector3f APP2N2N2 {1.0 / 2.0, -1.0 / 2.0, -2.0};
+constexpr XrVector3f APP2P2N2 {1.0 / 2.0, 1.0 / 2.0, -2.0};
+
+constexpr XrVector3f APZZN3 {0.0, 0.0, -3.0};
+
+constexpr XrVector3f APN2P6N2 {-1.0 / 2.0, 1.0 / 6.0, -2.0};
+constexpr XrVector3f APN6N2N2 {-1.0 / 6.0, -1.0 / 2.0, -2.0};
+constexpr XrVector3f APN6N6N2 {-1.0 / 6.0, -1.0 / 6.0, -2.0};
+constexpr XrVector3f APN6P6N2 {-1.0 / 6.0, 1.0 / 6.0, -2.0};
+constexpr XrVector3f APP6N6N2 {1.0 / 6.0, -1.0 / 6.0, -2.0};
+constexpr XrVector3f APP6P6N2 {1.0 / 6.0, 1.0 / 6.0, -2.0};
+constexpr XrVector3f APP6P2N2 {1.0 / 6.0, 1.0 / 2.0, -2.0};
+constexpr XrVector3f APP2N6N2 {1.0 / 2.0, -1.0 / 6.0, -2.0};
 
 constexpr Vertex gModelsVertices[] =
 {
 
 // arrow model start
-  { {-1.0 / 2.0, -1.0 / 2.0, -2.0}, NNPN, ColorDarkGrey}, { {1.0 / 2.0, -1.0 / 2.0, -2.0}, NNZZ, ColorDarkGrey}, { {0, 0, -3.0}, NZZP, ColorLightGrey}, // 3
-  { {1.0 / 2.0, -1.0 / 2.0, -2.0}, NNPN, ColorDarkGrey}, { {1.0 / 2.0, 1.0 / 2.0, -2.0}, NNZZ, ColorDarkGrey}, { {0, 0, -3.0}, NZZP, ColorLightGrey}, // 6
-  { {1.0 / 2.0, 1.0 / 2.0, -2.0}, NNPN, ColorDarkGrey}, { {-1.0 / 2.0, 1.0 / 2.0, -2.0}, NNZZ, ColorDarkGrey}, { {0, 0, -3.0}, NZZP, ColorLightGrey}, // 9
-  { {-1.0 / 2.0, 1.0 / 2.0, -2.0}, NNPN, ColorDarkGrey}, { {-1.0 / 2.0, -1.0 / 2.0, -2.0}, NNZZ, ColorDarkGrey}, { {0, 0, -3.0}, NZZP, ColorLightGrey}, // 12
+  {APN2N2N2, NNPN, ColorDarkGrey}, {APP2N2N2, NNZZ, ColorDarkGrey}, {APZZN3, NZZP, ColorLightGrey}, // 3
+  {APP2N2N2, NNPN, ColorDarkGrey}, {APP2P2N2, NNZZ, ColorDarkGrey}, {APZZN3, NZZP, ColorLightGrey}, // 6
+  {APP2P2N2, NNPN, ColorDarkGrey}, {APN2P2N2, NNZZ, ColorDarkGrey}, {APZZN3, NZZP, ColorLightGrey}, // 9
+  {APN2P2N2, NNPN, ColorDarkGrey}, {APN2N2N2, NNZZ, ColorDarkGrey}, {APZZN3, NZZP, ColorLightGrey}, // 12
 
-  { {-1.0 / 6.0, -1.0 / 2.0, -2.0}, NNPN, ColorGrey}, { {-1.0 / 2.0, -1.0 / 2.0, -2.0}, NNZZ, ColorGrey}, { {-1.0 / 2.0, 1.0 / 6.0, -2.0}, NZZP, ColorHalfBlue}, // 15
-  { {-1.0 / 2.0, 1.0 / 6.0, -2.0}, NNPN, ColorHalfBlue}, { {-1.0 / 6.0, 1.0 / 6.0, -2.0}, NNZZ, ColorHalfBlue}, { {-1.0 / 6.0, -1.0 / 2.0, -2.0}, NZZP, ColorGrey}, // 18
+  {APN6N2N2, NNPN, ColorGrey}, {APN2N2N2, NNZZ, ColorGrey}, {APN2P6N2, NZZP, ColorHalfBlue}, // 15
+  {APN2P6N2, NNPN, ColorHalfBlue}, {APN6P6N2, NNZZ, ColorHalfBlue}, {APN6N2N2, NZZP, ColorGrey}, // 18
 
-  { {-1.0 / 2.0, 1.0 / 6.0, -2.0}, NNPN, ColorGrey}, { {-1.0 / 2.0, 1.0 / 2.0, -2.0}, NNZZ, ColorGrey}, { {1.0 / 6.0, 1.0 / 2.0, -2.0}, NZZP, ColorHalfYellow}, // 21
-  { {1.0 / 6.0, 1.0 / 2.0, -2.0}, NNPN, ColorHalfYellow}, { {1.0 / 6.0, 1.0 / 6.0, -2.0}, NNZZ, ColorHalfYellow}, { {-1.0 / 2.0, 1.0 / 6.0, -2.0}, NZZP, ColorGrey}, // 24
+  {APN2P6N2, NNPN, ColorGrey}, {APN2P2N2, NNZZ, ColorGrey}, {APP6P2N2, NZZP, ColorHalfYellow}, // 21
+  {APP6P2N2, NNPN, ColorHalfYellow}, {APP6P6N2, NNZZ, ColorHalfYellow}, {APN2P6N2, NZZP, ColorGrey}, // 24
 
-  { {1.0 / 6.0, 1.0 / 2.0, -2.0}, NNPN, ColorGrey}, { {1.0 / 2.0, 1.0 / 2.0, -2.0}, NNZZ, ColorGrey}, { {1.0 / 2.0, -1.0 / 6.0, -2.0}, NZZP, ColorHalfCyan}, // 27
-  { {1.0 / 2.0, -1.0 / 6.0, -2.0}, NNPN, ColorHalfCyan}, { {1.0 / 6.0, -1.0 / 6.0, -2.0}, NNZZ, ColorHalfCyan}, { {1.0 / 6.0, 1.0 / 2.0, -2.0}, NZZP, ColorGrey}, // 30
+  {APP6P2N2, NNPN, ColorGrey}, {APP2P2N2, NNZZ, ColorGrey}, {APP2N6N2, NZZP, ColorHalfCyan}, // 27
+  {APP2N6N2, NNPN, ColorHalfCyan}, {APP6N6N2, NNZZ, ColorHalfCyan}, {APP6P2N2, NZZP, ColorGrey}, // 30
 
-  { {1.0 / 2.0, -1.0 / 6.0, -2.0}, NNPN, ColorGrey}, { {1.0 / 2.0, -1.0 / 2.0, -2.0}, NNZZ, ColorGrey}, { {-1.0 / 6.0, -1.0 / 2.0, -2.0}, NZZP, ColorQuarterPurple}, // 33
-  { {-1.0 / 6.0, -1.0 / 2.0, -2.0}, NNPN, ColorQuarterPurple}, { {-1.0 / 6.0, -1.0 / 6.0, -2.0}, NNZZ, ColorQuarterPurple}, { {1.0 / 2.0, -1.0 / 6.0, -2.0}, NZZP, ColorGrey}, // 36
+  {APP2N6N2, NNPN, ColorGrey}, {APP2N2N2, NNZZ, ColorGrey}, {APN6N2N2, NZZP, ColorQuarterPurple}, // 33
+  {APN6N2N2, NNPN, ColorQuarterPurple}, {APN6N6N2, NNZZ, ColorQuarterPurple}, {APP2N6N2, NZZP, ColorGrey}, // 36
 
   // -X
-  {APNNN, NNNN, ColorHalfBlue}, {APNZZ, NNZZ, ColorGrey}, {APNPN, NNPN, ColorHalfGreen}, {APNPN, NNPN, ColorHalfGreen}, {APNZZ, NNZZ, ColorGrey}, {APNPP, NNPP, ColorHalfRed}, // 42
-  {APNPP, NNPP, ColorHalfRed}, {APNZZ, NNZZ, ColorGrey}, {APNNP, NNNP, ColorHalfYellow}, {APNNP, NNNP, ColorHalfYellow}, {APNZZ, NNZZ, ColorGrey}, {APNNN, NNNN, ColorHalfBlue}, // 48
+  {APNNN, NNNN, ColorHalfBlue}, {APNZZ, NNZZ, ColorGrey}, {APNPN, NNPN, ColorHalfGreen}, {APNPN, NNPN, ColorHalfGreen}, {APNZZ, NNZZ, ColorGrey}, {APN6P6N2, NNPP, ColorHalfRed}, // 42
+  {APN6P6N2, NNPP, ColorHalfRed}, {APNZZ, NNZZ, ColorGrey}, {APN6N6N2, NNNP, ColorHalfYellow}, {APN6N6N2, NNNP, ColorHalfYellow}, {APNZZ, NNZZ, ColorGrey}, {APNNN, NNNN, ColorHalfBlue}, // 48
 
   // +X
-  {APPNN, NPNN, ColorHalfBlah}, {APPZZ, NPZZ, ColorGrey}, {APPNP, NPNP, ColorHalfBlah}, {APPNP, NPNP, ColorHalfBlah}, {APPZZ, NPZZ, ColorGrey}, {APPPP, NPPP, ColorHalfBlah}, // 54
-  {APPPP, NPPP, ColorHalfBlah}, {APPZZ, NPZZ, ColorGrey}, {APPPN, NPPN, ColorHalfBlah}, {APPPN, NPPN, ColorHalfBlah}, {APPZZ, NPZZ, ColorGrey}, {APPNN, NPNN, ColorHalfBlah}, // 60
+  {APPNN, NPNN, ColorHalfBlah}, {APPZZ, NPZZ, ColorGrey}, {APP6N6N2, NPNP, ColorHalfBlah}, {APP6N6N2, NPNP, ColorHalfBlah}, {APPZZ, NPZZ, ColorGrey}, {APP6P6N2, NPPP, ColorHalfBlah}, // 54
+  {APP6P6N2, NPPP, ColorHalfBlah}, {APPZZ, NPZZ, ColorGrey}, {APPPN, NPPN, ColorHalfBlah}, {APPPN, NPPN, ColorHalfBlah}, {APPZZ, NPZZ, ColorGrey}, {APPNN, NPNN, ColorHalfBlah}, // 60
 
   // -Y
-  {APNNN, NNNN, ColorHalfGreen}, {APZNZ, NZNZ, ColorGrey}, {APNNP, NNNP, ColorHalfGreen}, {APNNP, NNNP, ColorHalfGreen}, {APZNZ, NZNZ, ColorGrey}, {APPNP, NPNP, ColorHalfGreen}, // 66
-  {APPNP, NPNP, ColorHalfGreen}, {APZNZ, NZNZ, ColorGrey}, {APPNN, NPNN, ColorHalfGreen}, {APPNN, NPNN, ColorHalfGreen}, {APZNZ, NZNZ, ColorGrey}, {APNNN, NNNN, ColorHalfGreen}, // 72
+  {APNNN, NNNN, ColorHalfGreen}, {APZNZ, NZNZ, ColorGrey}, {APN6N6N2, NNNP, ColorHalfGreen}, {APN6N6N2, NNNP, ColorHalfGreen}, {APZNZ, NZNZ, ColorGrey}, {APP6N6N2, NPNP, ColorHalfGreen}, // 66
+  {APP6N6N2, NPNP, ColorHalfGreen}, {APZNZ, NZNZ, ColorGrey}, {APPNN, NPNN, ColorHalfGreen}, {APPNN, NPNN, ColorHalfGreen}, {APZNZ, NZNZ, ColorGrey}, {APNNN, NNNN, ColorHalfGreen}, // 72
 
   // +Y
-  {APNPP, NNPP, ColorHalfOrange}, {APZPZ, NZPZ, ColorGrey}, {APNPN, NNPN, ColorHalfOrange}, {APNPN, NNPN, ColorHalfOrange}, {APZPZ, NZPZ, ColorGrey}, {APPPN, NPPN, ColorHalfOrange}, // 78
-  {APPPN, NPPN, ColorHalfOrange}, {APZPZ, NZPZ, ColorGrey}, {APPPP, NPPP, ColorHalfOrange}, {APPPP, NPPP, ColorHalfOrange}, {APZPZ, NZPZ, ColorGrey}, {APNPP, NNPP, ColorHalfOrange}, // 84
+  {APN6P6N2, NNPP, ColorHalfOrange}, {APZPZ, NZPZ, ColorGrey}, {APNPN, NNPN, ColorHalfOrange}, {APNPN, NNPN, ColorHalfOrange}, {APZPZ, NZPZ, ColorGrey}, {APPPN, NPPN, ColorHalfOrange}, // 78
+  {APPPN, NPPN, ColorHalfOrange}, {APZPZ, NZPZ, ColorGrey}, {APP6P6N2, NPPP, ColorHalfOrange}, {APP6P6N2, NPPP, ColorHalfOrange}, {APZPZ, NZPZ, ColorGrey}, {APN6P6N2, NNPP, ColorHalfOrange}, // 84
 
   // -Z
   {APNPN, NNPN, ColorDarkGrey}, {APZZN, NZZN, ColorLightGrey}, {APNNN, NNNN, ColorDarkGrey}, {APNNN, NNNN, ColorDarkGrey}, {APZZN, NZZN, ColorLightGrey}, {APPNN, NPNN, ColorDarkGrey}, // 90
@@ -251,125 +259,5 @@ constexpr unsigned short gModelsIndices[] =
 // cube model finish
 
 };
-
-#if 0
-constexpr Vertex gModelsVertices[] =
-{
-  // -X
-  {PNPN, NNPN, ColorDarkGrey}, {PNZZ, NNZZ, ColorLightGrey}, {PNNN, NNNN, ColorDarkGrey}, {PNNN, NNNN, ColorDarkGrey}, {PNZZ, NNZZ, ColorLightGrey}, {PNNP, NNNP, ColorDarkGrey},
-  {PNPN, NNPN, ColorDarkGrey}, {PNPP, NNPP, ColorDarkGrey}, {PNZZ, NNZZ, ColorLightGrey}, {PNZZ, NNZZ, ColorLightGrey}, {PNPP, NNPP, ColorDarkGrey}, {PNNP, NNNP, ColorDarkGrey},
-
-  // +X
-  {PPPN, NPPN, ColorHalfBlue}, {PPNN, NPNN, ColorHalfBlue}, {PPZZ, NPZZ, ColorLightGrey}, {PPZZ, NPZZ, ColorLightGrey}, {PPNN, NPNN, ColorHalfBlue}, {PPNP, NPNP, ColorHalfBlue},
-  {PPPN, NPPN, ColorHalfBlue}, {PPZZ, NPZZ, ColorLightGrey}, {PPPP, NPPP, ColorHalfBlue}, {PPPP, NPPP, ColorHalfBlue}, {PPZZ, NPZZ, ColorLightGrey}, {PPNP, NPNP, ColorHalfBlue},
-
-  // -Y
-  {PNNN, NNNN, ColorHalfGreen}, {PNNP, NNNP, ColorHalfGreen}, {PZNZ, NZNZ, ColorLightGrey}, {PZNZ, NZNZ, ColorLightGrey}, {PNNP, NNNP, ColorHalfGreen}, {PPNP, NPNP, ColorHalfGreen},
-  {PNNN, NNNN, ColorHalfGreen}, {PZNZ, NZNZ, ColorLightGrey}, {PPNN, NPNN, ColorHalfGreen}, {PPNN, NPNN, ColorHalfGreen}, {PZNZ, NZNZ, ColorLightGrey}, {PPNP, NPNP, ColorHalfGreen},
-
-  // +Y
-  {PNPN, NNPN, ColorHalfRed}, {PPPN, NPPN, ColorHalfRed}, {PZPZ, NZPZ, ColorLightGrey}, {PZPZ, NZPZ, ColorLightGrey}, {PPPN, NPPN, ColorHalfRed}, {PPPP, NPPP, ColorHalfRed},
-  {PNPN, NNPN, ColorHalfRed}, {PZPZ, NZPZ, ColorLightGrey}, {PNPP, NNPP, ColorHalfRed}, {PNPP, NNPP, ColorHalfRed}, {PZPZ, NZPZ, ColorLightGrey}, {PPPP, NPPP, ColorHalfRed},
-
-  // -Z
-  {PNNN, NNNN, ColorHalfCyan}, {PPNN, NPNN, ColorHalfCyan}, {PZZN, NZZN, ColorLightGrey}, {PZZN, NZZN, ColorLightGrey}, {PPNN, NPNN, ColorHalfCyan}, {PPPN, NPPN, ColorHalfCyan},
-  {PNNN, NNNN, ColorHalfCyan}, {PZZN, NZZN, ColorLightGrey}, {PNPN, NNPN, ColorHalfCyan}, {PNPN, NNPN, ColorHalfCyan}, {PZZN, NZZN, ColorLightGrey}, {PPPN, NPPN, ColorHalfCyan},
-
-  // +Z
-  {PNNP, NNNP, ColorHalfYellow}, {PNPP, NNPP, ColorHalfYellow}, {PZZP, NZZP, ColorLightGrey}, {PZZP, NZZP, ColorLightGrey}, {PNPP, NNPP, ColorHalfYellow}, {PPPP, NPPP, ColorHalfYellow},
-  {PNNP, NNNP, ColorHalfYellow}, {PZZP, NZZP, ColorLightGrey}, {PPNP, NPNP, ColorHalfYellow}, {PPNP, NPNP, ColorHalfYellow}, {PZZP, NZZP, ColorLightGrey}, {PPPP, NPPP, ColorHalfYellow}
-};
-#endif
-
-#if 0
-constexpr Vertex gModelsVertices[] =
-{
-  // -X
-  {LTB, DarkRed}, {LBF, DarkRed}, {LBB, DarkRed}, {LTB, DarkRed}, {LTF, DarkRed}, {LBF, DarkRed},
-
-  // +X
-  {RTB, Red}, {RBB, Red}, {RBF, Red}, {RTB, Red}, {RBF, Red}, {RTF, Red},
-
-  // -Y
-  {LBB, DarkGreen}, {LBF, DarkGreen}, {RBF, DarkGreen}, {LBB, DarkGreen}, {RBF, DarkGreen}, {RBB, DarkGreen},
-
-  // +Y
-  {LTB, Green}, {RTB, Green}, {RTF, Green}, {LTB, Green}, {RTF, Green}, {LTF, Green},
-
-  // -Z
-  {LBB, DarkBlue}, {RBB, DarkBlue}, {RTB, DarkBlue}, {LBB, DarkBlue}, {RTB, DarkBlue}, {LTB, DarkBlue},
-
-  // +Z
-  {LBF, Blue}, {LTF, Blue}, {RTF, Blue}, {LBF, Blue}, {RTF, Blue}, {RBF, Blue}
-};
-#endif
-
-#if 0
-vec4(0.0, 0.0, 0.0, 1.0),
-vec4(0.0, 0.0, 0.0, 1.0),
-vec4(1.0, 1.0, 1.0, 1.0),
-vec4(0.0, 0.0, 0.0, 1.0),
-vec4(1.0, 1.0, 1.0, 1.0),
-vec4(0.0, 0.0, 0.0, 1.0),
-
-vec4(0.79, 0.79, 0.79, 1.0),
-vec4(0.25, 0.25, 0.25, 1.0),
-vec4(0.25, 0.25, 0.25, 1.0),
-vec4(0.79, 0.79, 0.79, 1.0),
-vec4(0.25, 0.25, 0.25, 1.0),
-vec4(0.25, 0.25, 0.25, 1.0),
-
-vec4(0.0, 1.0, 0.0, 1.0),
-vec4(1.0, 0.0, 0.0, 1.0),
-vec4(0.0, 1.0, 0.0, 1.0),
-vec4(0.0, 1.0, 0.0, 1.0),
-vec4(0.0, 1.0, 0.0, 1.0),
-vec4(0.0, 0.0, 1.0, 1.0),
-
-vec4(1.0, 0.0, 0.0, 1.0),
-vec4(0.0, 1.0, 0.0, 1.0),
-vec4(0.0, 0.0, 1.0, 1.0),
-vec4(1.0, 0.0, 0.0, 1.0),
-vec4(0.0, 0.0, 1.0, 1.0),
-vec4(0.0, 1.0, 0.0, 1.0),
-
-vec4(0.0, 0.54, 0.54, 1.0),
-vec4(0.0, 0.54, 0.54, 1.0),
-vec4(0.54, 0.54, 0.54, 1.0),
-vec4(0.0, 0.54, 0.54, 1.0),
-vec4(0.54, 0.54, 0.54, 1.0),
-vec4(0.0, 0.54, 0.54, 1.0),
-
-vec4(0.54, 0.54, 0.0, 1.0),
-vec4(0.54, 0.54, 0.0, 1.0),
-vec4(0.54, 0.54, 0.54, 1.0),
-vec4(0.54, 0.54, 0.0, 1.0),
-vec4(0.54, 0.54, 0.54, 1.0),
-vec4(0.54, 0.54, 0.0, 1.0)
-#endif
-
-#if 0
-// Winding order is clockwise. Each side uses a different color.
-constexpr unsigned short gModelsIndices[] =
-{
-  0,  1,  2,  3,  4,  5, // -X
-  6,  7,  8,  9,  10, 11,
-
-  12, 13, 14, 15, 16, 17, // +X
-  18, 19, 20, 21, 22, 23,
-
-  24, 25, 26, 27, 28, 29, // -Y
-  30, 31, 32, 33, 34, 35,
-
-  36, 37, 38, 39, 40, 41, // +Y
-  42, 43, 44, 45, 46, 47,
-
-  48, 49, 50, 51, 52, 53, // -Z
-  54, 55, 56, 57, 58, 59,
-
-  60, 61, 62, 63, 64, 65, // +Z
-  66, 67, 68, 69, 70, 71
-};
-#endif
 
 } // namespace Geometry
